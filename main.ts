9c56e54688c5c77c0baa202e6113d74d628ea70b
Jordan Poles <jpoles1@gmail.com>
2024-06-23 02:37:37 -0400

    idk

--- main.ts
+++ main.ts
@@ -1,134 +1,178 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Component, MarkdownPostProcessor, MarkdownRenderer, Plugin } from 'obsidian'
 
-// Remember to rename these classes and interfaces!
+const filenamePlaceholder = '%'
+const filenameExtensionPlaceholder = '%.%'
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+export default class ImageCaptions extends Plugin {
+  observer: MutationObserver
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+  async onload () {
+    this.registerMarkdownPostProcessor(
+      externalImageProcessor(this)
+    )
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+    this.observer = new MutationObserver((mutations: MutationRecord[]) => {
+      mutations.forEach((rec: MutationRecord) => {
+        if (rec.type === 'childList') {
+			(<Element>rec.target).querySelectorAll('.HyperMD-quote:has(> .image-embed)')
+			.forEach(async (gallery_block) => {
+				const img_list: HTMLElement[] = [];
+				const fig_list: HTMLElement[] = [];
+				gallery_block.querySelectorAll(".image-embed")
+				.forEach(async imageEmbedContainer => {
+					const img = imageEmbedContainer.querySelector('img,figure')
+					const width = imageEmbedContainer.getAttribute('width') || ''
+					const captionText = `${Date.now()}`
 
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+					if (!img) return
+					img_list.push(img)
+					const figure = imageEmbedContainer.querySelector('figure')
+					fig_list.push(figure!)
+					const figCaption = imageEmbedContainer.querySelector('figcaption')
+					if (figure || img.parentElement?.nodeName === 'FIGURE') {
+						// Node has already been processed
+						// Check if the text needs to be updated
+						if (figCaption && captionText) {
+						// Update the text in the existing element
+						const children = await renderMarkdown(captionText, '', this) ?? [captionText]
+						figCaption.replaceChildren(...children)
+						} else if (!captionText) {
+						// The alt-text has been removed, so remove the custom <figure> element
+						// and set it back to how it was originally with just the plain <img> element
+						imageEmbedContainer.appendChild(img)
+						figure?.remove()
+						}
+					} else {
+						if (captionText && captionText !== imageEmbedContainer.getAttribute('src')) {
+						await insertFigureWithCaption(img, imageEmbedContainer, captionText, '', this)
+						}
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+					if (width) {
+						// Update the image width, if specified
+						img.setAttribute('width', width)
+					} else {
+						// It's critical to remove the empty width attribute, rather than setting it to ""
+						img.removeAttribute('width')
+					}
+				})
+				if (fig_list.length > 0) {
+					await insertGallery(gallery_block, fig_list)
 				}
-			}
-		});
+			})
+		}
+      })
+    })
+    this.observer.observe(document.body, { subtree: true, childList: true })
+  }
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+  onunload () {
+    this.observer.disconnect()
+  }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+function insertGallery(gallery_container: HTMLElement | Element, image_list: HTMLElement[]) {
+	const gal = gallery_container.createEl('picture')
+	gal.addClass('image-gal')
+	console.log("list", image_list)
+	const left_arrow = gal.createEl('div')
+	left_arrow.innerHTML = '<div style="font-size: 200%"><</div>'
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	for (const img of image_list) {
+		gal.appendChild(img)
 	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+	console.log(gal)
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+/**
+ * Process an HTMLElement or Element to extract the caption text
+ * from the alt attribute.
+ *
+ * Optionally use the image filename if the filenamePlaceholder is specified.
+ *
+ * @param img
+ */
+function getCaptionText (img: HTMLElement | Element) {
+  let captionText = img.getAttribute('alt') || ''
+  const src = img.getAttribute('src') || ''
+  if (captionText === src) {
+    // If no caption is specified, then Obsidian puts the src in the alt attribute
+    captionText = ''
+  } else if (captionText === filenamePlaceholder) {
+    // Optionally use filename as caption text if the placeholder is used
+    const match = src.match(/[^\\/]+(?=\.\w+$)|[^\\/]+$/)
+    if (match?.[0]) {
+      captionText = match[0]
+    }
+  } else if (captionText === filenameExtensionPlaceholder) {
+    // Optionally use filename (including extension) as caption text if the placeholder is used
+    const match = src.match(/[^\\/]+$/)
+    if (match?.[0]) {
+      captionText = match[0]
+    }
+  } else if (captionText === '\\' + filenamePlaceholder) {
+    // Remove the escaping to allow the placeholder to be used verbatim
+    captionText = filenamePlaceholder
+  }
+  captionText = captionText.replace(/<<(.*?)>>/g, (match, linktext) => {
+    return '[[' + linktext + ']]'
+  })
+  return captionText
+}
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+/**
+ * External images can be processed with a Markdown Post Processor, but only in Reading View.
+ */
+function externalImageProcessor (plugin: ImageCaptions): MarkdownPostProcessor {
+  return (el, ctx) => {
+    el.findAll('img:not(.emoji)')
+      .forEach(async img => {
+        const captionText = getCaptionText(img)
+        const parent = img.parentElement
+        if (parent && parent?.nodeName !== 'FIGURE' && captionText && captionText !== img.getAttribute('src')) {
+          await insertFigureWithCaption(img, parent, captionText, ctx.sourcePath, plugin)
+        }
+      })
+  }
+}
 
-	display(): void {
-		const {containerEl} = this;
+/**
+ * Replace the original <img> element with this structure:
+ * @example
+ * <figure>
+ *   <img>
+ *   <figcaption>The caption text</figcaption>
+ * </figure>
+ *
+ * @param {HTMLElement} imageEl - The original image element to insert inside the <figure>
+ * @param {HTMLElement|Element} outerEl - Most likely the parent of the original <img>
+ * @param captionText
+ * @param sourcePath
+ * @param plugin
+ */
+async function insertFigureWithCaption (imageEl: HTMLElement, outerEl: HTMLElement | Element, captionText: string, sourcePath: string, plugin: ImageCaptions) {
+  const figure = outerEl.createEl('figure')
+  figure.addClass('image-captions-figure')
+  figure.appendChild(imageEl)
+  const children = await renderMarkdown(captionText, sourcePath, plugin) ?? [captionText]
+  figure.createEl('figcaption', {
+    cls: 'image-captions-caption'
+  }).replaceChildren(...children)
+}
 
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+/**
+ * Easy-to-use version of MarkdownRenderer.renderMarkdown. Returns only the child nodes, rather than a container block.
+ * @param markdown
+ * @param sourcePath
+ * @param component - Typically you can just pass the plugin instance, but Liam from the Obsidian team says
+ *   it's not a good practice (https://github.com/obsidianmd/obsidian-releases/pull/2263#issuecomment-1711864829).
+ *   I'm currently struggling to find a proper way to do it.
+ */
+export async function renderMarkdown (markdown: string, sourcePath: string, component: Component): Promise<NodeList | undefined> {
+  const el = createDiv()
+  await MarkdownRenderer.renderMarkdown(markdown, el, sourcePath, component)
+  for (const child of el.children) {
+    if (child.tagName.toLowerCase() === 'p') {
+      return child.childNodes
+    }
+  }
 }
